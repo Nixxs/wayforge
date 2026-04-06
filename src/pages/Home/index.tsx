21ad2b56c6bc6452938f6a86572534344f385bcb
Washburne\nicholasc <nicholas.chai@intelligis.io>
2026-04-06 11:41:18 +0800

    footer section created

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,8 @@ import Hero from "./Hero";
 import ComparisonSection from "./ComparisonSection";
 import FleetSection from "./FleetSection";
 import DestinationsSection from "./DestinationsSection";
+import FaqSection from "./FaqSection";
+import Footer from "./Footer";
 
 export default function Home() {
     return (
@@ -10,6 +12,8 @@ export default function Home() {
             <ComparisonSection />
             <FleetSection />
             <DestinationsSection />
+            <FaqSection />
+            <Footer />
         </>
     );
 }
